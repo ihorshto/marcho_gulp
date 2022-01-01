81918b226db0eb165d0409f5e7f5040c355e85a0
ihorshto <igorshtogrin2016@gmail.com>
2022-01-01 20:52:31 +0200

    added product

--- app/js/main.js
+++ app/js/main.js
@@ -7,4 +7,13 @@ $ (function(){
     autoplay: true
 
   });
+
+    $(".star").rateYo({
+      starWidth: "18px",
+      normalFill: "#ccccce",
+      ratedFill: "#ffc35b",
+      readOnly: true
+    });
+  
+
 });
